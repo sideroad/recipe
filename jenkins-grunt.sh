bb773bad5b25ef1ffa5b41114ca0e19c005a3cb5
sideroad <sideroad.jp@gmail.com>
2013-06-09 00:50:08 +0900

    npm install

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -1,4 +1,5 @@
 export PATH=$PATH:/Users/sideroad/.node/v0.8.6/bin:/usr/local/bin
 export NODE_PATH=/Users/sideroad/workspace/recipe/node_modules
 
-grunt jenkins
+npm install
+grunt jenkins --no-color
